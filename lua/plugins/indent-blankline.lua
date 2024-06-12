0f9ecfa8d1fa5f819de50542db143d7ccf5ad3f5
hyeonwoo <hykanghw@gmail.com>
2024-06-13 01:19:55 +0900

    rm json

--- lua/plugins/indent-blankline.lua
+++
@@ -1,4 +0,0 @@
--- Provides: indent-blankline
-return{
-  "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} 
-}
--- lua/plugins/nvim-autopairs.lua
+++
@@ -1,6 +0,0 @@
--- Provide a auto-pairs for neovim
-return{
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-}
