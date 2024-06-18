67c3fcae2b7456cd54a674d74f9bf693198a7dd5
hyeonwoo <hykanghw@gmail.com>
2024-06-19 03:25:53 +0900

    cmp Update-keymap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -39,7 +39,7 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(4),
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-Space>"] = cmp.mapping.complete(), -- Show completion suggestions
+          ["<C-s>"] = cmp.mapping.complete(), -- Show completion suggestions
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- <CR>:Carriage return  (Enter Key)
         }),
