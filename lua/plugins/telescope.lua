2ec58dbd8461fe3990e75d1dc70788c94685c329
hyeonwoo <hykanghw@gmail.com>
2024-06-19 20:38:58 +0900

    Feat: Noice add

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    
     config = function()
       local builtin = require("telescope.builtin")
       mapKeys('<leader>tf', builtin.find_files)  -- Find file in cwd.
