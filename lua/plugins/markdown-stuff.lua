f80002002f59e0f936c9681b4142d1b2f96a8beb
hyeonwoo <hykanghw@gmail.com>
2024-06-21 10:17:24 +0900

    Mod: Obsidian config

--- lua/plugins/markdown-stuff.lua
+++ lua/plugins/markdown-stuff.lua
@@ -42,8 +42,25 @@ return {
           -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
           -- In this case a note with the title 'My new note' will be given an ID that looks
           -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-          return tostring(os.time()) .. "-" .. suffix
+          local suffix = ""
+          if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+          end
+          return suffix
+        end,
+
+        -- Optional, alternatively you can customize the frontmatter data.
+        ---@return table
+        note_frontmatter_func = function(note)
+          -- Add the title of the note as an alias.
+          local out = { up = "", date = tostring(os.date("%Y-%m-%d")), tags = note.tags }
+          return out
         end,
 
         ui = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,37 +2,36 @@
 local mapKeys = require("utils.keyMapper").mapKey
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      mapKeys('<leader>tf', builtin.find_files)  -- Find file in cwd.
-      mapKeys('<leader>tg', builtin.live_grep)   -- Find string in cwd.
-      mapKeys('<leader>t*', builtin.grep_string) -- Find string under cursor in cwd
-      mapKeys('<leader>tb', builtin.buffers)     -- List of Recent.
-      mapKeys('<leader>th', builtin.help_tags)
-    end
+      mapKeys("<leader>tf", builtin.find_files) -- Find file in cwd.
+      mapKeys("<leader>tg", builtin.live_grep) -- Find string in cwd.
+      mapKeys("<leader>t*", builtin.grep_string) -- Find string under cursor in cwd
+      mapKeys("<leader>tb", builtin.buffers)  -- List of Recent.
+      mapKeys("<leader>th", builtin.help_tags)
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require('telescope').setup({
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
       })
       require("telescope").load_extension("ui-select")
-    end
+    end,
   },
   {
-      "nvim-telescope/telescope-file-browser.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     config = function()
       mapKeys("<space>fb", ":Telescope file_browser<CR>")
-      end
-  }
+    end,
+  },
 }
